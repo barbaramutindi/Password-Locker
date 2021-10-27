a19eef8a0955c1dfbbea512f302b44fff5e159eb
barbara mutindi <mutindibarbara@gmail.com>
2021-10-27 18:30:41 +0300

    test user

--- user_credentials_test.py
+++ user_credentials_test.py
@@ -1,3 +1,30 @@
 import unittest
 import pyperclip
 from user_credentials import User, Credential
+
+class TestUser(unittest.TestCase):
+	'''
+	Test class that defines test cases for the user class behaviours.
+	Args:
+	    unittest.TestCase: helps in creating test cases
+	'''
+	def setUp(self):
+		'''
+		Function to create a user account before each test
+		'''
+		self.new_user = User('Barbara','Mutindi','1234')
+
+	def test__init__(self):
+		'''
+		Test to if check the initialization/creation of user instances is properly done
+		'''
+		self.assertEqual(self.new_user.first_name,'Barbara')
+		self.assertEqual(self.new_user.last_name,'Mutindi')
+		self.assertEqual(self.new_user.password,'1234')
+
+	def test_save_user(self):
+		'''
+		Test to check if the new users info is saved into the users list
+		'''
+		self.new_user.save_user()
+		self.assertEqual(len(User.users_list),1)
